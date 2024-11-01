9c70d67beb8763ec8ca5c3adc007c38283981f37
MatiasCardoso000 <cardosomatias.000@gmail.com>
2024-11-01 15:35:52 -0300

    probando estilos

---
+++ pages/CartPage/script.js
@@ -0,0 +1,3 @@
+const cart = document.querySelector(".cart-list");
+
+export const createCart = (products) => {};
--- script.js
+++ script.js
@@ -1,56 +1,57 @@
+import { createCart } from "./pages/CartPage/script.js";
+
 const input = document.querySelector("input");
 const form = document.querySelector("form");
 const productsList = document.querySelector(".products-list-grid");
 
 let product = "";
 
+input.addEventListener("input", ({ target }) => {
+  product = target.value;
+});
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  getProducts();
 });
-
-input.addEventListener("change", ({ target }) => {
-  product = target.value;
-  console.log(product);
-});
-
-const url = `https://fakestoreapi.com/products/${product}`;
 
 async function getProducts() {
+  const url = `https://fakestoreapi.com/products`;
+
   try {
     const response = await fetch(url);
     const result = await response.json();
-    console.log(result);
 
     showProducts(result);
+    return result;
   } catch (error) {
     console.error(error);
   }
 }
 
 function showProducts(products) {
+  productsList.innerHTML = "";
   const filteredProducts = filterProducts(products, product);
 
   filteredProducts.map((product) => {
     const product_title = document.createElement("p");
     const li = document.createElement("li");
     const img = document.createElement("img");
+    const add_btn = document.createElement("button");
+
     product_title.textContent = product.title;
     img.setAttribute("src", product.image);
 
-    li.append(product_title, img);
-    li.classList = "product";
+    li.append(product_title, img, add_btn);
+    li.classList = "product ";
     productsList.append(li);
   });
 }
 
 function filterProducts(products, product) {
-  products.filter((p) => {
-    if (p.title.toLocaleLowerCase().includes(product.toLocaleLowerCase())) {
-      return p;
-    }
-  });
-
-  return products;
+  return products.filter((p) =>
+    p.title.toLocaleLowerCase().includes(product.toLocaleLowerCase())
+  );
 }
 
 window.addEventListener("DOMContentLoaded", getProducts);
