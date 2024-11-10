2a3345a8509056268ce458171923367bec2c2c4c
MatiasCardoso4 <cardosomatias1994@gmail.com>
2024-11-10 20:24:34 -0300

    Add .htaccess for permissions policy and update cart page styles and functionality

--- pages/CartPage/cart.js
+++ pages/CartPage/cart.js
@@ -1,5 +1,4 @@
 const cartList = document.querySelector(".cart-list");
-const total_products = document.querySelector(".amount");
 
 const savedStorage = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
@@ -7,25 +6,17 @@ const savedStorage = localStorage.getItem("cart")
 
 let cart = savedStorage || [];
 
-let totalProducts = localStorage.getItem("totalProducts");
-
 export const addToCart = (product) => {
   const ifExist = cart.some((p) => p.id === product.id);
   if (!ifExist) {
     cart.push({ ...product, quantity: 1 });
-    totalProductsCount();
   } else {
     cart.forEach((p) => {
       p.id === product.id && p.quantity++;
     });
-
-    totalProducts = cart.reduce((acc, product) => {
-      return acc + product.quantity;
-    }, 0);
   }
 
   localStorage.setItem("cart", JSON.stringify(cart));
-  localStorage.setItem("totalProducts", totalProducts);
   createCart();
 };
 
@@ -35,12 +26,13 @@ const removeFromCart = (product) => {
   if (ifExist) {
     cart.forEach((p) => {
       p.id === product.id && p.quantity--;
-      totalProductsCount();
     });
+    cart = cart.reduce((acc, curr) => {
+      acc + curr.price;
+    }, 0);
   }
 
   localStorage.setItem("cart", JSON.stringify(cart));
-  localStorage.setItem("totalProducts", totalProducts);
   createCart();
 };
 
@@ -48,16 +40,7 @@ const deleteFromCart = (id) => {
   cart = cart.filter((p) => p.id !== id);
   localStorage.setItem("cart", JSON.stringify(cart));
 
-  totalProductsCount();
   createCart();
-};
-
-const totalProductsCount = () => {
-  totalProducts = cart.reduce((acc, product) => {
-    return acc + product.quantity;
-  }, 0);
-
-  localStorage.setItem("totalProducts", totalProducts);
 };
 
 export const createCart = () => {
@@ -68,7 +51,8 @@ export const createCart = () => {
     const product_name = document.createElement("p");
     const img = document.createElement("img");
     const delete_btn = document.createElement("button");
-    const span = document.createElement("span");
+    const productQuantity = document.createElement("span");
+    const productPriceTag = document.createElement("span");
     const add_btn = document.createElement("button");
     const remove_btn = document.createElement("button");
     remove_btn.className = "remove-btn";
@@ -77,7 +61,8 @@ export const createCart = () => {
     add_btn.addEventListener("click", () => addToCart(p));
     remove_btn.addEventListener("click", () => removeFromCart(p));
     remove_btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-440v-80h560v80H200Z"/></svg>`;
-    span.textContent = `x${p.quantity}`;
+    productQuantity.textContent = `x${p.quantity}`;
+    productPriceTag.textContent = `$${p.price}`;
     delete_btn.className = "delete-btn";
     delete_btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#f50505"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`;
     delete_btn.addEventListener("click", () => deleteFromCart(p.id));
@@ -85,8 +70,15 @@ export const createCart = () => {
     img.className = "product-image";
     li.className = "product-cart";
     product_name.textContent = p.title;
-    total_products.textContent = totalProducts;
-    li.append(img, product_name, span, add_btn, remove_btn, delete_btn);
+    li.append(
+      img,
+      product_name,
+      productPriceTag,
+      productQuantity,
+      add_btn,
+      remove_btn,
+      delete_btn
+    );
     cartList.appendChild(li);
   });
 };
