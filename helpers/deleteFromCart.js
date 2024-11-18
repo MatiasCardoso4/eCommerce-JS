28aa5abf2348d340146761e24e18adf855bc7ebc
MatiasCardoso4 <cardosomatias1994@gmail.com>
2024-11-17 23:58:52 -0300

    Fix cart deletion logic and update script paths: ensure localStorage is updated after cart modification; remove unnecessary border in cart styles; adjust script imports for category menu in CartPage and HomePage

--- helpers/deleteFromCart.js
+++ helpers/deleteFromCart.js
@@ -2,10 +2,10 @@ import { cart, createCart } from "../pages/CartPage/cart.js";
 
 export const deleteFromCart = (id) => {
   const updatedCart = cart.filter((p) => p.id !== id);
-  localStorage.setItem("cart", JSON.stringify(updatedCart));
 
   cart.length = 0;
   cart.push(...updatedCart);
+  localStorage.setItem("cart", JSON.stringify(updatedCart));
 
   createCart();
 };
